14c63c6fcf789db764583d4b156de4ccd3473e88
daantascaio <caiodantasl.lara@gmail.com>
2023-09-23 19:11:40 -0300

    bump

--- QApplication_QPushButton_.py
+++ QApplication_QPushButton_.py
@@ -4,12 +4,18 @@
 # PySide6.QtWidgets -> Onde estão os widgets do PySide6
 import sys
 
-from PySide6.QtWidgets import QApplication, QPushButton
+from PySide6.QtWidgets import QApplication, QPushButton, QWidget
 
 app = QApplication(sys.argv)
 
 button = QPushButton('Texto do Botão')
-button.setStyleSheet('font-size: 40px')
+button.setStyleSheet('font-size: 80px')
 button.show()  # Adicionar o widget na hierarquia e exibe a janela
+
+button2 = QPushButton('Texto do botão 2')
+button2.setStyleSheet('font-size: 40px; background: red;')
+button2.show()
+
+central_widget = QWidget()
 
 app.exec()  # Loop da aplicação
