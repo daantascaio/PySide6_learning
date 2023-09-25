1fd0da154aa46dfef4f2ec1c779169b4e45869d6
Caio Dantas <caiodantasl.lara@gmail.com>
2023-09-25 17:20:51 -0300

    create: display and configuration them

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import sys
 from main_window import MainWindow
+from display import Display
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QLineEdit
 from variables import WINDOW_ICON_PATH
 
 if __name__ == '__main__':
@@ -14,6 +15,11 @@ if __name__ == '__main__':
     print(icon)
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
+
+    # Display
+    display = Display()
+    # display.setPlaceholderText('Write thing')
+    window.addToVLayout(display)
 
     # Executa tudo
     window.adjustFixedSize()
--- src/variables.py
+++ src/variables.py
@@ -3,3 +3,12 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent.parent
 FILES_DIR = ROOT_DIR / 'files'
 WINDOW_ICON_PATH = FILES_DIR / 'icon.png'
+
+
+# Sizing
+BIG_FONT_SIZE = 40
+MEDIUM_FONT_SIZE = 24
+SMALL_FONT_SIZE = 18
+HEIGHT_TEXT_SIZE = int(BIG_FONT_SIZE + (BIG_FONT_SIZE / 3))
+TEXT_MARGIN = 15
+MINIMUM_WIDTH = 500
