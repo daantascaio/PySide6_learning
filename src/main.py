467bfb8db4dfc1178f5e3cea99327fda981bbba7
Caio Dantas <caiodantasl.lara@gmail.com>
2023-09-25 17:44:17 -0300

    saving progess

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import sys
+
 from main_window import MainWindow
 from display import Display
+from info import Info
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication, QLineEdit
 from variables import WINDOW_ICON_PATH
@@ -15,6 +17,10 @@ if __name__ == '__main__':
     print(icon)
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
+
+    # Info
+    info = Info('2 + 2 = 4')
+    window.addToVLayout(info)
 
     # Display
     display = Display()
