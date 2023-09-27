1377615cb372a6b53d95c68a97b5126dfe178c82
daantascaio <caiodantasl.lara@gmail.com>
2023-09-27 12:50:13 -0300

    def validNumber

--- src/buttons_.py
+++ src/buttons_.py
@@ -1,7 +1,7 @@
 from PySide6.QtWidgets import QPushButton, QGridLayout
 from PySide6.QtCore import Slot
 from variables import MEDIUM_FONT_SIZE
-from utils import isNumOrDot, isEmpty
+from utils import isNumOrDot, isEmpty, isValidNumber
 from display import Display
 
 
@@ -16,6 +16,7 @@ class Button(QPushButton):
         self.setFont(font)
         self.setMinimumSize(75, 75)
         self.setCheckable(True)
+        # self.setContentsMargins(0, 0, 0, 0)
 
 
 class ButtonsGrid(QGridLayout):
@@ -70,5 +71,10 @@ class ButtonsGrid(QGridLayout):
         return realSlot
 
     def _insertButtonTextToDisplay(self, button):
-        button_text = button.text()
-        self.display.insert(button_text)
+        buttonText = button.text()
+        newDisplayValue = self.display.text() + buttonText
+
+        if not isValidNumber(newDisplayValue):
+            return
+
+        self.display.insert(buttonText)
