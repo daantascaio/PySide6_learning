567269fbab9c0657aa9d9881fd80da2dce321f1e
Caio Dantas <caiodantasl.lara@gmail.com>
2023-10-05 14:54:47 -0300

    go, go, GO

--- src/buttons_.py
+++ src/buttons_.py
@@ -32,7 +32,7 @@ class ButtonsGrid(QGridLayout):
         super().__init__(*args, **kwargs)
 
         self._gridMask = [
-            ['C', 'D', '^', '/'],
+            ['C', 'DEL', '^', '/'],
             ['7', '8', '9', '*'],
             ['4', '5', '6', '-'],
             ['1', '2', '3', '+'],
@@ -93,7 +93,7 @@ class ButtonsGrid(QGridLayout):
         if text == 'C':
             self._connectButtonClicked(button, self._clear)
 
-        if text == 'D':
+        if text == 'DEL':
             self._connectButtonClicked(button, self.display.backspace)
 
         if text in '+-/*^':
